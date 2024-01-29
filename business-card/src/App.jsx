e255ba4284268afbf1ccada94ae84cfab2445c95
amber Do <anhdosheridan@gmail.com>
2024-01-28 22:29:35 -0500

    first commit

---
+++ business-card/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Info from './components/info/info.js'
+import './App.css'
+
+function App() {
+  return (
+    <Info></Info>
+  )
+}
+
+export default App
---
+++ business-card/src/components/info/info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import avatar from '../../assets/rei.jpeg';
+
+const Info = () => (
+  <div>
+    <img src={avatar} alt="Avatar" />
+  </div>
+);
+
+export default Info;
